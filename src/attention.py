d32aa26ba0f7cc6638cb1cf0878372cb69b3add4
EC2 Default User <ec2-user@ip-172-31-5-160.us-west-1.compute.internal>
2020-05-04 01:53:39 +0000

    fix attention resnet dimension mismatch

--- src/attention.py
+++ src/attention.py
@@ -18,23 +18,25 @@ class AttBottleneck(Bottleneck):
         # Replace conv2 in the bottleneck with attention.
         width = int(planes * (base_width / 64.)) * groups
         self.att2 = attention(width, num_heads) 
+        if stride == 1:
+            self.avgpool_after_att = nn.Identity()
+        else:
+            self.avgpool_after_att = nn.AvgPool1d(kernel_size=3, stride=stride,
+                    padding=dilation)
 
     def forward(self, x):
         identity = x
-        print(x.shape)
 
         out = self.conv1(x)
         out = self.bn1(out)
         out = self.relu(out)
-        print(out.shape)
 
         out = out.permute(2, 0, 1)
         out, _ = self.att2(out, out, out)
-        print(out.shape)
         out = out.permute(1, 2, 0)
+        out = self.avgpool_after_att(out)
         out = self.bn2(out)
         out = self.relu(out)
-        print(out.shape)
 
         out = self.conv3(out)
         out = self.bn3(out)
